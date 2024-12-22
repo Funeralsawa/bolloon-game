4e30ed39ddb27fa1241416b6a6a5a68488964565
Funerals <ox7zcdbb@gmail.com>
2024-12-22 17:18:00 +0800

    add menu playground

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -4,8 +4,58 @@ class AcGameMenu {
         this.root = root;
         this.$menu = $(`
             <div class="ac-game-menu">
+                <div class="ac-game-menu-field">
+                    <div class="ac-game-menu-field-item ac-game-menu-field-item-single-mode">
+                        单人模式
+                    </div>
+                    <br>
+                    <div class="ac-game-menu-field-item ac-game-menu-field-item-multi-mode">
+                        多人模式
+                    </div>
+                    <br>
+                    <div class="ac-game-menu-field-item ac-game-menu-field-item-settings">
+                        设置
+                    </div>
+                </div>
             </div>
         `);
        this.root.$ac_game.append(this.$menu); 
+       this.$single = this.$menu.find('.ac-game-menu-field-item-single-mode');
+       this.$multi = this.$menu.find('.ac-game-menu-field-item-multi-mode');
+       this.$settings = this.$menu.find('.ac-game-menu-field-item-settings');
+
+       this.start();
+    }
+
+    start() {
+        this.add_listening_events();
+    }
+
+    add_listening_events() {
+        let outer = this;
+
+        this.$single.click(function(){
+            console.log('click single mode');
+            outer.hide();
+            outer.root.playground.show();
+        });
+
+        this.$multi.click(function(){
+            console.log('click multi mode');
+        });
+
+        this.$settings.click(function(){
+            console.log('click settings');
+        });
+    }
+
+    show() {
+        //显示菜单
+        this.$menu.show();
+    }
+
+    hide() {
+        //关闭菜单
+        this.$menu.hide();
     }
 }
