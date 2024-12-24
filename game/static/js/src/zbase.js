86fd9d9cbaec9570a53cf6a113458e94b6c23540
Funerals <ox7zcdbb@gmail.com>
2024-12-24 18:57:23 +0800

    player fireball

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,9 +1,9 @@
-class AcGame {
+export class AcGame {
     constructor(id) {
         console.log("create ac game");
         this.id = id;
         this.$ac_game = $('#' + this.id);
-        this.menu = new AcGameMenu(this);
+        //this.menu = new AcGameMenu(this);
         this.playground = new AcGamePlayground(this);
 
         this.start();
