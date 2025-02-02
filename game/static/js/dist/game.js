6ec5a29e4123847ca46be09c3714206c2ebd977e
Funerals <ox7zcdbb@gmail.com>
2025-02-02 17:18:04 +0800

    finish login and logout

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -14,7 +14,7 @@ class AcGameMenu {
                     </div>
                     <br>
                     <div class="ac-game-menu-field-item ac-game-menu-field-item-settings">
-                        设置
+                        退出
                     </div>
                 </div>
             </div>
@@ -47,6 +47,7 @@ class AcGameMenu {
 
         this.$settings.click(function(){
             console.log('click settings');
+            outer.root.settings.logout_on_remote();
         });
     }
 
@@ -500,11 +501,58 @@ class Settings {
                     </div>
                 </div>
                 <div class="ac-game-settings-register">
+                    <div class="ac-game-settings-title">注册</div>
+                        <div class="ac-game-settings-username">
+                            <div class="ac-game-settings-item">
+                                <input type="text" placeholder="用户名">
+                            </div>
+                        </div>
+                        <div class="ac-game-settings-password ac-game-settings-password-first">
+                            <div class="ac-game-settings-item">
+                                <input type="password" placeholder="密码">
+                            </div>
+                        </div>
+                        <div class="ac-game-settings-password ac-game-settings-password-second">
+                            <div class="ac-game-settings-item">
+                                <input type="password" placeholder="确认密码">
+                            </div>
+                        </div>
+                        <div class="ac-game-settings-submit">
+                            <div class="ac-game-settings-item">
+                                <button>注册</button>
+                            </div>
+                        </div>
+                        <div class="ac-game-settings-error-messages">
+                            <!--用户名密码错误 -->
+                        </div>
+                        <div class="ac-game-settings-option">
+                            登录
+                        </div>
+                        <br>
+                        <div class="ac-game-settings-acwing">
+                            <img width="30" src="https://app6916.acapp.acwing.com.cn/static/image/settings/acwing_logo.png">
+                            <br>
+                            <div>AcWing一键登录</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         `);
         this.$login = this.$settings.find(".ac-game-settings-login");
+        this.$login_username = this.$login.find(".ac-game-settings-username input");
+        this.$login_password = this.$login.find(".ac-game-settings-password input");
+        this.$login_submit = this.$login.find(".ac-game-settings-submit button");
+        this.$login_error_messages = this.$login.find(".ac-game-settings-error-messages");
+        this.$login_option = this.$login.find(".ac-game-settings-option");
+
         this.$register = this.$settings.find(".ac-game-settings-register");
+        this.$register_username = this.$register.find(".ac-game-settings-username input");
+        this.$register_password = this.$register.find(".ac-game-settings-password-first input");
+        this.$register_password_confirm = this.$register.find(".ac-game-settings-password-second input");
+        this.$register_submit = this.$register.find(".ac-game-settings-submit button");
+        this.$register_error_messages = this.$register.find(".ac-game-settings-error-messages");
+        this.$register_option = this.$register.find(".ac-game-settings-option");
+
         this.$login.hide();
         this.$register.hide();
         this.root.$ac_game.append(this.$settings);
@@ -513,11 +561,78 @@ class Settings {
 
     start() {
         this.getinfo();
+        this.add_listening_events();
+    }
+
+    add_listening_events() {
+        this.add_listening_events_login();
+        this.add_listening_events_register();
+    }
+
+    add_listening_events_login() {
+        let outer = this;
+        this.$login_option.on("click", function() {
+            outer.register();
+        });
+        this.$login_submit.click(function() {
+            outer.login_on_remote();
+        });
+    }
+
+    add_listening_events_register() {
+        let outer = this;
+        this.$register_option.on("click", function() {
+            outer.login();
+        })
     }
 
     login() {  //打开登录界面
         this.$register.hide();
         this.$login.show();
+    }
+
+    login_on_remote() { //登录远程服务器
+        let outer = this;
+        let username = this.$login_username.val();
+        let password = this.$login_password.val();
+        this.$login_error_messages.empty();
+
+        $.ajax({
+            url: "https://app6916.acapp.acwing.com.cn/settings/login/",
+            type: "GET",
+            data: {
+                username: username,
+                password: password,
+            },
+            success: function(resp) {
+                console.log(resp);
+                if(resp.result === "success") {
+                    location.reload();
+                }
+                else {
+                    outer.$login_error_messages.html(resp.result);
+                }
+            }
+        });
+    }
+
+    register_on_remote() {  //在远程服务器上注册 
+
+    }
+
+    logout_on_remote() {    //在远程服务器上登出
+        if(this.platform === "ACAPP") return false;
+
+        $.ajax({
+            url: "https://app6916.acapp.acwing.com.cn/settings/logout/",
+            type: "GET",
+            success: function(resp) {
+                console.log(resp);
+                if(resp.result === "success") {
+                    location.reload();
+                }
+            }
+        });
     }
 
     register() {    //打开注册界面
@@ -542,6 +657,7 @@ class Settings {
                     outer.hide();
                     outer.root.menu.show();
                 } else {
+                    // outer.register();
                     outer.login();
                 }
             }
